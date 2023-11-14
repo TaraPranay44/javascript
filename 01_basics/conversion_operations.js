fbd6669e17a46c82478eac4003752a625117fd8e
TaraPranay44 <tarapranay9.7@gmail.com>
2023-11-14 19:37:45 +0530

    changes

---
+++ 01_basics/conversion_operations.js
@@ -0,0 +1,72 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let value = Number(score);
+// console.log(typeof value)
+// console.log(value)
+
+// console.log("\n");
+
+// let score2 = null;
+
+// console.log(typeof score2);
+// console.log(typeof(score2));
+
+// let value2 = Number(score2);
+// console.log(typeof value2)
+// console.log(value2)
+
+
+// console.log("\n");
+
+// let score3 = true;
+
+// console.log(typeof score3);
+// console.log(typeof(score3));
+
+// let value3 = Number(score3);
+// console.log(typeof value3)
+// console.log(value3)
+
+// console.log("\n");
+
+// let score4 = undefined;
+
+// console.log(typeof score4);
+// console.log(typeof(score4));
+
+// let value4 = Number(score4);
+// console.log(typeof value4)
+// console.log(value4)
+
+// Boolean(var)
+// String(var) etc are other types
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/4);
+// console.log(5%3);
+
+// console.log("tara" + 1);
+// console.log("2" + 1);
+// console.log(2+ "1");
+// console.log("tara" + 1 + 2);
+// console.log("3" + 1 + 2);
+// console.log(3 + "1" + 2);
+// console.log(3 + 1 + "2");  not a good method
+
+// console.log(+"");
+// console.log(+true);
+// console.log(+undefined);
+
+console.log("2" > 1);
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log("2" == 2);
+
+console.log("2" === 2);
---
+++ 01_basics/test.js
@@ -0,0 +1,5 @@
+let tara = 10
+khushi = 20
+
+console.log(tara + khushi);
+console.log('Hello World');
