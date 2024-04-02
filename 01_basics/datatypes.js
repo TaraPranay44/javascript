c05002bc59ce131c5aad3c75933e1089219e0c9a
Tara Pranay Kancharlapalli <108684840+TaraPranay44@users.noreply.github.com>
2024-04-02 13:32:11 +0000

    khush

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,8 +1,17 @@
 // primitive data types :
 
-let str = "tara"
-let num = 32
-let bool = true
-let nulVal = null
-let empty = undefined
+let str = "tara"    //string
+let num = 32   //number
+let bool = true        //boolean
+let nulVal = null      //null
+let empty = undefined    //undefined
+                         // symbol (unique)
+
+// non-primitive
+
+//object
+console.log(typeof(null));  // its type of object
+console.log(typeof(undefined));   //but its a type of undefined itslef, as undefined is not an object
+
+
 
