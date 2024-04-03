7628607cdc5fa4db1c6c819f7e07cb93e2281e97
Tara Pranay Kancharlapalli <108684840+TaraPranay44@users.noreply.github.com>
2024-04-03 13:59:49 +0000

    khsuh

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -1,13 +1,17 @@
 // let score = "33abc";    
 
-// console.log(typeof score);
-// console.log(typeof(score));
+// console.log(typeof score); //string
+// console.log(typeof(score)); //string
 
 // let value = Number(score);
-// console.log(typeof value)
-// console.log(value)
+// console.log(typeof value)  //number
+// console.log(value)     //NaN      22abc is not a pure number , it may be converted to number , but its value will become NaN
+//if score = NULL    // its prints 0
+//if score = undefined // it prints NaN(not a number) 
+//if score = bool     //0 for false,1 for true
 
-// console.log("\n");
+// funny thing here is typeof(NaN) is a number
+// console.log("\n");   // prints new line
 
 // let score2 = null;
 
@@ -23,7 +27,7 @@
 
 // let score3 = true;
 
-// console.log(typeof score3);
+// console.log(typeof score3); 
 // console.log(typeof(score3));
 
 // let value3 = Number(score3);
@@ -51,17 +55,17 @@
 // console.log(2/4);
 // console.log(5%3);
 
-// console.log("tara" + 1);
-// console.log("2" + 1);
-// console.log(2+ "1");
+// console.log("tara" + 1); //
+// console.log("2" + 1); //21
+// console.log(2+ "1");   //21
 // console.log("tara" + 1 + 2);
-// console.log("3" + 1 + 2);
-// console.log(3 + "1" + 2);
-// console.log(3 + 1 + "2");  not a good method
+// console.log("3" + 1 + 2); //312 
+// console.log(3 + "1" + 2);  //312
+// console.log(3 + 1 + "2");  not a good method // 42
 
-// console.log(+"");
-// console.log(+true);
-// console.log(+undefined);
+// console.log(+""); //0
+// console.log(+true);  //1
+// console.log(+undefined);  //NaN  
 
 console.log("2" > 1);
 console.log(null > 0);
