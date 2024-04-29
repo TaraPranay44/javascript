e23fb4f678a59fdef41dc359667906a60193f4a1
Tara Pranay Kancharlapalli <108684840+TaraPranay44@users.noreply.github.com>
2024-04-29 14:30:53 +0000

    khush

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,11 @@
+function addTwoNumber(number1,number2){
+    return number1 + number2;
+}
+
+const r1 = addTwoNumber(1,2);
+const r2 = addTwoNumber(1,"2");
+const r3 = addTwoNumber(1,null);
+
+console.log(r1);
+console.log(r2);
+console.log(r3);
